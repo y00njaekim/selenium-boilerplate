92551a2e6204ad4f584281b4880d18cffe88ef94
y00njae <rladbswo12@gist.ac.kr>
2024-01-16 20:40:42 +0900

    Docker 파일 레이어 크롤링

--- application.py
+++ application.py
@@ -9,85 +9,26 @@ from selenium.common.exceptions import NoSuchElementException
 from crawl import any_of_elements_present
 
 
-def get_access_token(crawler, email, passwd):
-    url = "https://accounts.fitbit.com/login?targetUrl=https%3A%2F%2Fdev.fitbit.com%2Flogin%2Ftransferpage%3Fredirect%3Dhttps%253A%252F%252Fdev.fitbit.com%252Fapps"
+def crawl(crawler):
+    url = "https://hub.docker.com/layers/pytorch/pytorch/0.4_cuda9_cudnn7/images/sha256-2a25af68b37a33881e6e41dfa663291e39fac784d9453dfa26be918fb57d25e5?context=explore"
     crawler.get_url(url)
+    
     WebDriverWait(crawler.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="password"]'))
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler'))
     )
-
-    email_input = crawler.crawl_item(
-        By.CSS_SELECTOR, 'input[type="email"]', is_preprocess=False
-    )
-    email_input.clear()
-    email_input.send_keys(email)
-    time.sleep(1)
-
-    password_input = crawler.crawl_item(
-        By.CSS_SELECTOR, 'input[type="password"]', is_preprocess=False
-    )
-    password_input.clear()
-    password_input.send_keys(passwd)
-    time.sleep(1)
-
-    password_input.send_keys(Keys.ENTER)
-
+    crawler.crawl_item(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+    
     WebDriverWait(crawler.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "a.name"))
+        EC.presence_of_element_located((By.XPATH, '//div[@data-testid="imglayersLayerListItem"]'))
     )
-
-    application = crawler.crawl_item(By.CSS_SELECTOR, "a.name")
-    application.click()
-
-    app_property_blocks = crawler.crawl_items(By.CSS_SELECTOR, "div.appPropertyBlock")
-
-    client_id = ""
-    client_secret = ""
-    for block in app_property_blocks:
-        name_elem = block.find_element(By.CLASS_NAME, "name")
-        value_elem = block.find_element(By.CLASS_NAME, "value")
-
-        if name_elem.text == "OAuth 2.0 Client ID":
-            client_id = value_elem.text
-        elif name_elem.text == "Client Secret":
-            client_secret = value_elem.text
-
-    token_url = f"https://www.fitbit.com/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=http://127.0.0.1:8080/&expires_in=31536000&scope=activity+nutrition+heartrate+location+nutrition+profile+settings+sleep+social+weight"
-    crawler.get_url(token_url)
-
-    WebDriverWait(crawler.driver, 10).until(
-        any_of_elements_present(
-            [
-                (By.CSS_SELECTOR, "div#offline-resources"),
-                (By.CSS_SELECTOR, "span.allow-all-checkbox"),
-            ]
-        )
-    )
-
-    try:
-        allow_check_box = crawler.crawl_item(By.CSS_SELECTOR, "span.allow-all-checkbox")
-        allow_check_box.click()
-        allow_button = crawler.crawl_item(By.CSS_SELECTOR, "button.allow-button")
-        allow_button.click()
-        WebDriverWait(crawler.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div#offline-resources"))
-        )
-    except NoSuchElementException:
-        pass
-
-    token = crawler.driver.current_url
-    token = token.split("access_token=")[1].split("&")[0]
-
-    logout_url = "https://dev.fitbit.com/apps"
-    crawler.get_url(logout_url)
-    WebDriverWait(crawler.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#cust > ul > li.last > a"))
-    )
-
-    logout_button = crawler.crawl_item(By.CSS_SELECTOR, "#cust > ul > li.last > a")
-    logout_button.click()
-    WebDriverWait(crawler.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="password"]'))
-    )
-
-    return client_id, client_secret, token
+    
+    layers = crawler.crawl_items(By.XPATH, '//div[@data-testid="imglayersLayerListItem"]')
+    for layer in layers:
+        layer.click()
+        detail = crawler.crawl_item(By.XPATH, '//div[@data-testid="imglayersLayerInstruction"]/div[2]/div/div')
+        print("Layer: ")
+        print(layer.find_element(By.XPATH, 'div[2]').text.strip())
+        print("Command: ")
+        print(detail.text.strip())
+        print()
+        time.sleep(1)
--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 from crawl import WebCrawler
-from application import get_access_token
+from application import crawl
 from excel import read_excel
 
 
 def main():
     crawler = WebCrawler()
-
-    file_path = "../Fitbit_device_외주용.xlsx"
-    df = read_excel(file_path)
-
-    try:
-        df[["CLIENT_ID", "CLIENT_SECRET", "ACCESS_TOKEN"]] = df.apply(
-            lambda row: get_access_token(crawler, row["Google_id"], row["Google_pw"]),
-            axis=1,
-            result_type="expand",
-        )
-        df.to_excel(file_path, index=False)
-    except Exception as e:
-        print(e)
-
-    df.to_excel(file_path, index=False)
+    crawl(crawler)
     crawler.close()
 
 
